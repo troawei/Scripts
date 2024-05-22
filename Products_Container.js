7a69c5ee519e73f1f1c223b796173abcc02babc0
troawei <143855679+troawei@users.noreply.github.com>
2024-05-22 10:51:18 +0800

    Updated Products_Container.js

--- Products_Container.js
+++ Products_Container.js
@@ -1,52 +1,52 @@
 export const products = [{
-    img: 'IMGs/Chocolate Cupcake.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Chocolate%20Cupcake.png?raw=true',
     itemName: 'Chocolate Cupcake',
     itemPrice: 100,
     quantity: 1,
     itemDesc: 'Treat Yourself to a Symphony of Sweetness, Where Melting Chocolate and Buttery Softness Unite for an Unforgettable Taste.'
 },{
-    img: 'IMGs/Pastillas.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Pastillas.png?raw=true',
     itemName: 'Pastillas',
     itemPrice: 89,
     itemDesc: 'A Sweet Symphony of Tradition. Embrace the Rich Heritage and Savor the Irresistible Sweetness of Pastillias.'
 },{
-    img: 'IMGs/Dark Cake.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Dark%20Cake.png?raw=true',
     itemName: 'Dark Cake',
     itemPrice: 200,
     quantity: 1,
     itemDesc: 'Indulge in Every Bite, as Moist Cake Meets Silky Ganache, Elevating Your Taste Experience to New Heights of Indulgence.'
 },{
-    img: 'IMGs/Brownies.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Brownies.png?raw=true',
     itemName: 'Brownies',
     itemPrice: 100,
     quantity: 1,
     itemDesc: 'Experience the Perfect Balance of Fudgy Goodness and Gooey Chocolate, Crafting Moments of Pure Bliss with Every Bite.'
 },{
-    img: 'IMGs/Pulboron.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Pulboron.png?raw=true',
     itemName: 'Pulboron',
     itemPrice: 59,
     quantity: 1,
     itemDesc: 'Experience the Perfect Harmony of Buttery Texture and Sweetness, Crafted with Care to Delight Your Senses in Every Bite.'
 },{
-    img: 'IMGs/Dark Chocolate.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Dark%20Chocolate.png?raw=true',
     itemName: 'Dark Chocolate Truffles',
     itemPrice: 100,
     quantity: 1,
     itemDesc: 'Immerse Yourself in the Richness of Deep Cocoa Flavors, Tempered with Subtle Sweetness. Elevate Your Palate with Each Luxurious Bite.'
 },{
-    img: 'IMGs/Kape Truffles.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Kape%20Truffles.png?raw=true',
     itemName: 'Kape Truffles',
     itemPrice: 100,
     quantity: 1,
     itemDesc: 'Where Rich Coffee Meets Luscious Chocolate. Energize Your Day with the Perfect Blend of Robust Coffee Flavor and Decadent Chocolate Indulgence.'
 },{
-    img: 'IMGs/Waffle.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Waffle.png?raw=true',
     itemName: 'Waffle',
     itemPrice: 100,
     quantity: 1,
     itemDesc: 'Whether for Breakfast or a Sweet Afternoon Treat, Let Our Waffles Transport You to a World of Pure Delight and Satisfaction.'
 },{
-    img: 'IMGs/Classic Truffles.png',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Classic%20Truffles.png?raw=true',
     itemName: 'Classic Truffles',
     itemPrice: 89,
     quantity: 1,
@@ -56,63 +56,63 @@ export const products = [{
 
 export const drinkProducts = [{
     category: 'coffee',
-    img: 'IMGs/Americano.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Americano.jpg?raw=true',
     itemName: 'Americano',
     itemPrice: 89,
     quantity: 1,
     itemDesc: 'A smooth blend of rich espresso and creamy steamed milk, topped with a light froth. Perfect for a balanced coffee experience.'
 },{
     category: 'coffee',
-    img: 'IMGs/Cappuccino.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Cappuccino.jpg?raw=true',
     itemName: 'Cappuccino',
     itemPrice: 149,
     quantity: 1,
     itemDesc: 'A perfect balance of bold espresso, steamed milk, and a thick layer of frothy foam. Ideal for a robust and creamy coffee experience.'
 },{
     category: 'coffee',
-    img: 'IMGs/Espresso.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Espresso.jpg?raw=true',
     itemName: 'Espresso',
     itemPrice: 99,
     quantity: 1,
     itemDesc: 'A concentrated shot of rich, bold coffee with a thick crema on top. Its perfect for a quick, intense coffee experience.'
 },{
     category: 'nonCoffee',
-    img: 'IMGs/Healthy Chai Latte.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Healthy%20Chai%20Latte.jpg?raw=true',
     itemName: 'Healthy Chai Latte',
     itemPrice: 149,
     quantity: 1,
     itemDesc: 'infused with aromatic spices and crafted with nutritious almond or oat milk. Savor the comforting warmth and rich flavors guilt-free.'
 },{
     category: 'coffee',
-    img: 'IMGs/Latte.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Latte.jpg?raw=true',
     itemName: 'Latte',
     itemPrice: 139,
     quantity: 1,
     itemDesc: 'A smooth blend of rich espresso and creamy steamed milk, topped with a light froth. Perfect for a balanced coffee experience.'
 },{
     category: 'coffee',
-    img: 'IMGs/Macchiato.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Macchiato.jpg?raw=true',
     itemName: 'Macchiato',
     itemPrice: 129,
     quantity: 1,
     itemDesc: 'A rich espresso with a dollop of foamed milk on top, creating a perfect blend of strong coffee flavor and a hint of creaminess.'
 },{
     category: 'nonCoffee',
-    img: 'IMGs/Orange Julius.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Orange%20Julius.jpg?raw=true',
     itemName: 'Orange Julius',
     itemPrice: 99,
     quantity: 1,
     itemDesc: 'Savor the tangy sweetness of freshly squeezed oranges blended with creamy vanilla. A refreshing treat to invigorate your taste buds.'
 },{
     category: 'nonCoffee',
-    img: 'IMGs/Root Beer Float.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Root%20Beer%20Float.jpg?raw=true',
     itemName: 'Root Beer Float',
     itemPrice: 129,
     quantity: 1,
     itemDesc: 'Marrying creamy vanilla ice cream with the effervescence of root beer for a refreshing, nostalgic treat.'
 },{
     category: 'nonCoffee',
-    img: 'IMGs/Strawberry Acai.jpg',
+    img: 'https://github.com/troawei/Scripts/blob/main/IMGs/Strawberry%20Acai.jpg?raw=true',
     itemName: 'Strawberry Acai',
     itemPrice: 169,
     quantity: 1,
